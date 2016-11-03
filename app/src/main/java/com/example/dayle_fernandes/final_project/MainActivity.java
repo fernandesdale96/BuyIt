9d875aaad553850d889abbd4532d40956dc9fed4
Aditya <adityakrishnasharma@gmail.com>
2016-11-04 02:02:14 +0800

    Get all products(Not functional)

--- app/src/main/java/com/example/dayle_fernandes/final_project/MainActivity.java
+++ app/src/main/java/com/example/dayle_fernandes/final_project/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.dayle_fernandes.final_project;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,6 +12,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -22,6 +25,8 @@ import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import static java.lang.Math.*;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.NameValuePair;
 
 import com.example.dayle_fernandes.final_project.R;
 //import com.google.android.gms.analytics.ecommerce.Product;
@@ -31,6 +36,7 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import static com.example.dayle_fernandes.final_project.R.id.toolbar;
 
@@ -49,6 +55,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SimpleAdapter;
+
+import org.apache.http.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -58,6 +71,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     RecyclerView.Adapter aAdapter;
     testProductList list;
     MainActivity selfRef=this;
+   // JSONParser jParser = new JSONParser();
+    ArrayList<ProductInfo> productsList;
+    // url to get all products list
+    private static String url_all_products = "http://localhost/FinalProject/get_all_products";
+
+    private ProgressDialog pDialog;
+    private static final String PRODUCTS = "products";
+    private static final String ID= "pid";
+    private static final String NAME= "name";
+    private static final String PRICE= "price";
+    private static final String LOCATION= "location";
+    private static final String DISTANCE= "distance";
+    private static final String DESCRIPTION= "description";
+    private static final String CREATED_AT= "created_at";
+    private static final String UPDATED_AT= "updated_at";
+
+    JSONArray products=null;
+    JSONObject obj;
+   // ArrayList<HashMap<String, String>> productList = new ArrayList<HashMap<String, String>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,12 +121,143 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         aRecyclerView = (RecyclerView) findViewById(R.id.productlist_recyclerView);
         aRecyclerView.setHasFixedSize(true);
-        aAdapter = new ProductAdapter(list.getProducts());
+
+
+        // Hashmap for ListView
+        productsList = new ArrayList<ProductInfo>();
+
+        new GetQuestions().execute();
+
+        // Loading products in Background Thread
+        //new LoadAllProducts().execute();
+
+        // Creating JSON Parser object
+        //JSONParser jParser = new JSONParser();
+
+
+
+
+
+
+
+
+
+
+
+        //aAdapter = new ProductAdapter(list.getProducts());
         aLayoutManager = new LinearLayoutManager(this);
         aRecyclerView.setLayoutManager(aLayoutManager);
-        aRecyclerView.setAdapter(aAdapter);
-        aAdapter.notifyDataSetChanged();
+       // aRecyclerView.setAdapter(aAdapter);
+       // aAdapter.notifyDataSetChanged();
     }
+
+    private class GetQuestions extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(selfRef);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            // Creating service handler class instance
+            ServiceHandler sh = new ServiceHandler();
+            String jsonStr = sh.makeServiceCall(url_all_products, ServiceHandler.GET);
+            Log.d("Response: ", "> " + jsonStr);
+
+            if (jsonStr != null) {
+                try{
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                   products = jsonObj.getJSONArray(PRODUCTS);
+
+
+
+                    // looping through All Questions
+                    for(int i = 0; i < products.length(); i++){
+                        JSONObject c = products.getJSONObject(i);
+
+
+                        String id = c.getString(ID);
+                        String name= c.getString(NAME);
+                        String price = c.getString(PRICE);
+                        String loc = c.getString(LOCATION);
+                        String dist = c.getString(DISTANCE);
+                        ProductInfo p=new ProductInfo(name,Double.parseDouble(price),Double.parseDouble(dist),loc);
+
+
+                        productsList.add(p);
+                       /* // creating new HashMap
+                        HashMap<String, String> map = new HashMap<String, String>();
+
+                        // adding each child node to HashMap key => value
+                        map.put(ID, id);
+                        map.put(NAME, name);
+                        map.put(PRICE, price);
+                        map.put(LOCATION, loc);
+                        map.put(DISTANCE, dist);
+                        // adding HashList to ArrayList
+                        productList.add(map);
+                       */
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e("ServiceHandler", "Couldn't get any data from the url");
+            }
+
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+
+            aAdapter=new ProductAdapter(productsList);
+            aRecyclerView.setAdapter(aAdapter);
+            aAdapter.notifyDataSetChanged();
+          /*  ListAdapter adapter = new SimpleAdapter(
+                    getActivity(),
+                    questionsList,
+                    ask.code.R.layout.question_item,
+                    new String[] {
+                            TAG_NAME,
+                            TAG_TITLE,
+                            TAG_TIME},
+                    new int[] {
+                            ask.code.R.id.asker_name,
+                            ask.code.R.id.question_title,
+                            ask.code.R.id.question_rating});
+
+
+            setListAdapter(adapter);  */
+
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
 
 
     @Override

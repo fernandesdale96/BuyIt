27f38fd0d13bf5aa2aebc3980569ee96d44fac4d
fernandesdale96 <fernandesdale96@gmail.com>
2016-11-05 19:21:46 +0800

    Fixed Login Activity

--- app/src/main/java/com/example/dayle_fernandes/final_project/LoginActivity.java
+++ app/src/main/java/com/example/dayle_fernandes/final_project/LoginActivity.java
@@ -7,9 +7,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
@@ -21,8 +23,16 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.vision.text.Text;
 
-
+import static android.app.Activity.RESULT_OK;
+import static com.example.dayle_fernandes.final_project.R.id.btn_login;
+import static com.example.dayle_fernandes.final_project.R.id.intent_action;
+import static com.example.dayle_fernandes.final_project.R.id.start;
+import static com.google.android.gms.analytics.internal.zzy.h;
+import static com.google.android.gms.analytics.internal.zzy.i;
+import static com.google.android.gms.analytics.internal.zzy.n;
+import static com.google.android.gms.analytics.internal.zzy.p;
 
 
 /**
@@ -40,9 +50,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private Button btnlogin;
     private EditText email;
     private EditText pass;
+    private TextView signup;
     private String name;
     private String profile_url;
     private String email_id;
+    private String password;
+    private boolean google_flag = false;
 
 
     @Override
@@ -52,11 +65,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         email = (EditText) findViewById(R.id.input_email);
         pass = (EditText) findViewById(R.id.input_password);
-        btnlogin = (Button) findViewById(R.id.btn_login);
+        btnlogin = (Button) findViewById(btn_login);
         btngoogle = (SignInButton) findViewById(R.id.btn_google);
+        signup = (TextView) findViewById(R.id.link_signup);
 
         btngoogle.setOnClickListener(this);
-        btngoogle.setOnClickListener(this);
+        btnlogin.setOnClickListener(this);
+        signup.setOnClickListener(this);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -66,9 +81,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
-    private void signIn() {
+    private void google_login() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, REQUEST_SIGNIN);
+        google_flag = true;
     }
 
     private void handleSignInResult(GoogleSignInResult result) {
@@ -84,21 +100,102 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
 
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+
         }
     }
 
-    private void logIn() {
+    private void btn_login() {
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+        btnlogin.setEnabled(false);
+
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setIndeterminate(true);
+            mProgressDialog.setMessage("Authenticating...");
+            mProgressDialog.show();
+
+            email_id = email.getText().toString();
+            password = pass.getText().toString();
+
+            //TODO: Implement Authentication Logic
+
+            new android.os.Handler().postDelayed(
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            onLoginSuccess();
+                            mProgressDialog.dismiss();
+                        }
+                    }, 3000);
+        }
+
 
     }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login Failed", Toast.LENGTH_LONG).show();
+        btnlogin.setEnabled(true);
+    }
+
+    public boolean validate(){
+        boolean valid = true;
+
+        String email_text = email.getText().toString();
+        String password_text = pass.getText().toString();
+
+        if(email_text.isEmpty() || Patterns.EMAIL_ADDRESS.matcher(email_text).matches()){
+            email.setError("Please Enter a Valid Email ID");
+            valid = false;
+        }
+        else{
+            email.setError(null);
+        }
+
+        if(password_text.isEmpty() || password_text.length() < 4 || password_text.length() > 10){
+            pass.setError("Must be between 4 and 10 alphanumeric characters");
+            valid = false;
+        }
+        else{
+            pass.setError(null);
+        }
+        return valid;
+    }
+
+    public void onLoginSuccess() {
+        btnlogin.setEnabled(true);
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_SIGNIN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
 
+        if (google_flag == true) {
+            if (requestCode == REQUEST_SIGNIN) {
+                GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+                handleSignInResult(result);
+
+            }
+        } else {
+            if (requestCode == REQUEST_SIGNIN) {
+                //TODO: Add Login here
+                this.finish();
+            }
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
     }
 
     @Override
@@ -124,7 +221,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private void showProgressDialog() {
         if (mProgressDialog == null) {
             mProgressDialog = new ProgressDialog(this);
-            mProgressDialog.setMessage("Loading");
+            mProgressDialog.setMessage("Authenticating with Google");
             mProgressDialog.setIndeterminate(true);
         }
         mProgressDialog.show();
@@ -146,16 +243,25 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
+    public void signUp(){
+        Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
     @Override
     public void onClick(View v) {
         int id = v.getId();
 
         switch (id) {
-            case R.id.btn_login:
-                logIn();
+            case btn_login:
+                btn_login();
                 break;
             case R.id.btn_google:
-                signIn();
+                google_login();
+                break;
+            case R.id.link_signup:
+                signUp();
                 break;
 
         }
